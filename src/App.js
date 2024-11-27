b006ed62f379389b6a20a07ef14182c375add62f
MHH77 <mesfandiari77@gmail.com>
2024-11-27 18:02:25 +0330

    [feature] Add Bootstrap

--- src/App.js
+++ src/App.js
@@ -3,21 +3,35 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='mt-5 container'>
+      <div className='card'>
+        <div className='card-header' >
+          your todos
+        </div>
+        <div>
+          <table class='table table-hover'>
+            <thead>
+              <tr>
+                <th scope='col'>#</th>
+                <th scope='col'>Description</th>
+                <th scope='col'>Assigned</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th scope='row'>1</th>
+                <th>Feed dog</th>
+                <th>Mohammadhosein</th>
+              </tr>
+              <tr>
+                <th scope='row'>2</th>
+                <th>Get haircut</th>
+                <th>Mohammadhosein</th>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
     </div>
   );
 }
