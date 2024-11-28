8780f8bd01f94cf321908d4ce14c1b8c1a498af8
MHH77 <mesfandiari77@gmail.com>
2024-11-28 11:41:57 +0330

    [enhancement] Upgrade componnet - reusable

--- src/components/TodoRowItem.js
+++ src/components/TodoRowItem.js
@@ -1,9 +1,13 @@
 function TodoRowItem() {
+
+    const rowNumber = 1;
+    const rowDescription='feed dog';
+    const rowAssigned='Mohammadhosein';
     return (
         <tr>
-            <th scope='row'>1</th>
-            <th>feed dog</th>
-            <th>Mohammadhosein</th>
+            <th scope='row'>{rowNumber}</th>
+            <th>{rowDescription}</th>
+            <th>{rowAssigned}</th>
         </tr>
 
     )
