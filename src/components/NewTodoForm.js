295ebd268121e139667260c2d1cb3305526b680a
MHH77 <mesfandiari77@gmail.com>
2024-12-12 11:42:42 +0330

    [feature] Add state to NewTodoForm component with simple onchange action

--- src/components/NewTodoForm.js
+++ src/components/NewTodoForm.js
@@ -1,16 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 
 function NewTodoForm() {
+    const [description, setDescription] = useState('');
+    const [assigned, setAssigned] = useState('');
+
+    const descriptionCahnge = (event) => {
+        console.log(event.target.value);
+    }
+
+    const assignedCahnge = (event) => {
+        console.log(event.target.value);
+    }
+
     return (
         <div className="mt-5">
             <form>
                 <div className="mb-3">
                     <label className="form-label">Assigned</label>
-                    <input type="text" className="form-control" required></input>
+                    <input
+                        type="text"
+                        className="form-control"
+                        onChange={assignedCahnge}
+                        required>
+                    </input>
                 </div>
                 <div className="mb-3">
                     <label className="form-label">Description</label>
-                    <textarea className="form-control" rows={3} required></textarea>
+                    <textarea
+                        className="form-control"
+                        rows={3}
+                        onChange={descriptionCahnge}
+                        required>
+
+                    </textarea>
                 </div>
                 <button type="button" className="btn btn-primary mt-3">Add Todo</button>
             </form>
