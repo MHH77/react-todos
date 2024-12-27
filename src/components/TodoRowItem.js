a12afc998a706f04fcc50208a6a3f9b43117513e
MHH77 <mesfandiari77@gmail.com>
2024-12-27 14:03:51 +0330

    [bug] Fix bug

--- src/components/TodoRowItem.js
+++ src/components/TodoRowItem.js
@@ -1,7 +1,6 @@
 function TodoRowItem(props) {
 
     return (
-        //
         <tr onClick={() => props.deleteTodo(props.rowNumber)}>
             <th scope='row'>{props.rowNumber}</th>
             <th>{props.rowDescription}</th>
